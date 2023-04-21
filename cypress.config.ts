036d58cf548e76868b10156979dc5d4b660d003f
Rigers Leka <rigers.leka@ortec-finance.com>
2023-04-21 11:52:38 +0200

    configured cypress even for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
-  screenshotsFolder: "cypress/screenshots",
-  videosFolder: "cypress/videos",
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
 
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    retries: 3,
+  },
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 });
