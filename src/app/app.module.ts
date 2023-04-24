2fa1650d7153da1908fcda9264a438babebd4af7
Rigers Leka <rigers.leka@ortec-finance.com>
2023-04-24 17:08:26 +0200

    wip: material integration & impl into our components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { ContactDetailContainer } from './components/contact-detail-container/contact-detail-container';
 import { ContactDetailForm } from './components/contact-detail-form/contact-detail-form';
 import { ContactDetailForms } from './components/contact-detail-forms/contact-detail-forms';
 import { ContactDetailSearch } from './components/contact-detail-search/contact-detail-search.component';
-import { ContactDetailService } from './service/contact-detail.service';
 import { HeaderComponent } from './components/header/header.component';
-import { NgModule } from '@angular/core';
+import { ContactDetailService } from './service/contact-detail.service';
 
 @NgModule({
   declarations: [
@@ -25,6 +33,15 @@ import { NgModule } from '@angular/core';
     ReactiveFormsModule,
     FormsModule,
     HeaderComponent,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
   ],
   providers: [ContactDetailService],
   bootstrap: [AppComponent],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
