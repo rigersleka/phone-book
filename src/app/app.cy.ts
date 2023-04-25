ba65914807d0ab3d17715b7107b3942dd547162f
Rigers Leka <rigers.leka@ortec-finance.com>
2023-04-25 13:02:24 +0200

    quick refactor

--- src/app/app.cy.ts
+++ src/app/app.cy.ts
@@ -1,13 +1,14 @@
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 describe(AppComponent.name, () => {
   it('mounts', () => {
     cy.mount(AppComponent);
   });
 
-  it('verify that WelcomeComponent is called', () => {
-    cy.mount(AppComponent);
-    cy.get('[data-cy="welcome-component"]');
+  it('verify that HeaderComponent is called', () => {
+    cy.mount(HeaderComponent);
+    cy.get('[data-cy="header-component"]');
   });
 
   it('verify that ContactDetailContainer is called', () => {
