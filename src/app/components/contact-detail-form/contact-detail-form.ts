6efcf21fd6826c382531572c12da159613f8568a
Rigers Leka <rigersleka2@gmail.com>
2023-10-15 18:21:27 +0200

    integrate FormBuilder insead of FormGroup

--- src/app/components/contact-detail-form/contact-detail-form.ts
+++ src/app/components/contact-detail-form/contact-detail-form.ts
@@ -1,10 +1,10 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  inject,
-} from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Observable, of } from 'rxjs';
 
 import { IContactDetail } from 'src/app/models/contact-detail.model';
@@ -16,15 +16,14 @@ import { ContactDetailService } from '../../service/contact-detail.service';
   styleUrls: ['./contact-detail-form.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactDetailForm implements OnInit {
+export class ContactDetailForm {
   private contactDetailService = inject(ContactDetailService);
-  contactDetailForm!: FormGroup;
   searchForm!: FormGroup;
   contactDetail: IContactDetail;
   initialGenders = of(['Male', 'Female']); //mock data of selection
   genderItems$: Observable<string[]>;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.contactDetail = {} as IContactDetail;
     this.genderItems$ = this.getGenderItems();
   }
@@ -33,27 +32,23 @@ export class ContactDetailForm implements OnInit {
     return this.initialGenders;
   }
 
-  ngOnInit() {
-    this.contactDetailForm = new FormGroup({
-      firstName: new FormControl<string>('', [
-        Validators.required,
-        Validators.minLength(2),
-      ]),
-      phoneNumber: new FormControl<string>('', [
-        Validators.required,
-        Validators.pattern('^((\\+31-?)|0)?[0-9]{10}$'),
-      ]),
-      gender: new FormControl<string>('', [Validators.required]),
-    });
-  }
+  contactDetailForm = this.fb.group({
+    firstName: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    phoneNumber: new FormControl<string>('', [
+      Validators.required,
+      Validators.pattern('^((\\+31-?)|0)?[0-9]{10}$'),
+    ]),
+    gender: new FormControl<string>('', [Validators.required]),
+  });
 
   onGenderSelected(value: string) {
     console.log('The Gender selected is: ' + value);
   }
 
   savePhoneBook(): void {
-    this.contactDetail = this.contactDetailForm.value;
-    console.log('Contact Details:', this.contactDetail);
     this.contactDetailService.addPhoneBook({
       firstName: this.contactDetail.firstName,
       phoneNumber: this.contactDetail.phoneNumber,
