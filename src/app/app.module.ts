6841ae14b3679f482a28fb4fa2335bf624773379
Rigers Leka <rigersleka2@gmail.com>
2023-10-15 18:22:59 +0200

    ng-container DOM changes is those dictated by directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,7 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { ContactDetailContainer } from './components/contact-detail-container/contact-detail-container';
-import { ContactDetailForm } from './components/contact-detail-form/contact-detail-form';
-import { ContactDetailForms } from './components/contact-detail-forms/contact-detail-forms';
-import { ContactDetailSearch } from './components/contact-detail-search/contact-detail-search.component';
-import { ContactDetailService } from './service/contact-detail.service';
-import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoadingComponent } from './components/loading/loading.component';
-import { LoadingService } from './service/loading.service';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -21,9 +10,22 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactDetailContainer } from './components/contact-detail-container/contact-detail-container';
+import { ContactDetailForm } from './components/contact-detail-form/contact-detail-form';
+import { ContactDetailForms } from './components/contact-detail-forms/contact-detail-forms';
+import { ContactDetailSearch } from './components/contact-detail-search/contact-detail-search.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoadingComponent } from './components/loading/loading.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { AlphabeticalPipe } from './pipes/alphabetical.pipe';
+import { ContactDetailService } from './service/contact-detail.service';
+import { LoadingService } from './service/loading.service';
 import { MessagesService } from './service/messages.service';
-import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -34,6 +36,7 @@ import { NgModule } from '@angular/core';
     ContactDetailSearch,
     LoadingComponent,
     MessagesComponent,
+    AlphabeticalPipe,
   ],
   imports: [
     BrowserModule,
